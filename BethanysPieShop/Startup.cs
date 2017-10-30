3cc476ee61691c456c96ec192384ff47721acdae
debuckj <debuckj@gmail.com>
2017-10-30 14:27:12 +0100

    ASP.NET Core 2.0 MVC (1-4)

--- BethanysPieShop/Startup.cs
+++ BethanysPieShop/Startup.cs
@@ -6,6 +6,8 @@ using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
+using BethanysPieShop.Models;
+
 
 namespace BethanysPieShop
 {
@@ -15,6 +17,13 @@ namespace BethanysPieShop
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddTransient<ICategoryRepository, MockCategoryRepository>();
+            services.AddTransient<IPieRepository, MockPieRepository>();
+            // other options
+            //services.AddSingleton();
+            //services.AddScoped();
+
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -22,13 +31,18 @@ namespace BethanysPieShop
         {
             if (env.IsDevelopment())
             {
-                app.UseDeveloperExceptionPage();
+                app.UseDeveloperExceptionPage();    // showing expections in browser
+                app.UseStatusCodePages();           // handle response codes 400-600
+                app.UseStaticFiles();               // selfexpl
+                app.UseMvcWithDefaultRoute();       // later
             }
 
+            /*
             app.Run(async (context) =>
             {
                 await context.Response.WriteAsync("Hello World!");
             });
+            */
         }
     }
 }
